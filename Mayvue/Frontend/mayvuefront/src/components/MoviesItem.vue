2c17f32660685133b1b164cb4824376e5c25c373
Zach Everett <zacheverett@ZachBookPro.local>
2023-01-03 15:37:16 -0500

    Added table formatting to front end, very rough. Still very developer art-y

--- Mayvue/Frontend/mayvuefront/src/components/MoviesItem.vue
+++ Mayvue/Frontend/mayvuefront/src/components/MoviesItem.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <h1>Test</h1>
-    <div v-for="item in movies" :key="item.title">
-      <h1>{{ item.title }}</h1>
-    </div>
+    <h1>Movies</h1>
+    <table class="table">
+      <tr>
+        <th>Title</th>
+        <th>Release Year</th>
+        <th>Description</th>
+        <th>Actions</th>
+      </tr>
+      <tr v-for="item in movies" :key="item.title" class="row">
+        <td>{{ item.title }} </td>
+        <td> {{ item.releaseYear }} </td>
+        <td> {{ item.description }} </td>
+        <td>
+          <i class="fa-solid fa-pen-to-square" id="edit"></i>
+          <i class="fa-regular fa-copy" id="copy"></i>
+          <i class="fa-solid fa-trash" id="delete"></i>
+        </td>
+        
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -32,5 +48,25 @@ methods: {
 </script>
 
 <style>
+.table {
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: auto;
+  height: auto;
+  width: 40%;
+  border-collapse: collapse;
+  
+}
 
+tr {
+  border-bottom: 1px solid black;
+
+}
+
+#edit{
+  margin-right: 12%;
+}
+#copy {
+  margin-right: 12%;
+}
 </style>
